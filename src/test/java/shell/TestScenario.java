80e82847408619394b207c0fcd3d3f001047fe8d
CJH <jae637@naver.com>
2025-06-10 14:41:26 +0900

    [test] UT 껍데기만 만듦

---
+++ src/test/java/shell/TestScenario.java
@@ -0,0 +1,25 @@
+package shell;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestScenario {
+    TestShell testShell;
+
+
+    @Test
+    void FullWriteAndReadCompare_첫번째_시나리오테스트(){
+
+    }
+
+    @Test
+    void PartialLBAWrite_두번째_시나리오테스트(){
+
+    }
+
+    @Test
+    void WriteReadAging_세번째_시나리오테스트(){
+
+    }
+}
