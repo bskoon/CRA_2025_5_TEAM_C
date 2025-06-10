f1e8b481ebb29e8a40e81cf3b7b8bbf4b55f34aa
Jinchan <ghkdwlscks@gmail.com>
2025-06-10 17:01:58 +0900

    [refactoring]writeMethod

--- src/main/java/ssd/SSD.java
+++ src/main/java/ssd/SSD.java
@@ -1,6 +1,7 @@
 package ssd;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -8,37 +9,37 @@ import java.util.List;
 
 public class SSD {
 
+    public static final String SSD_NAND = "ssd_nand.txt";
+
     public SSD() {
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter("ssd_nand.txt"))) {
+        if (new File(SSD_NAND).exists()) return;
+        initSsdNandFile();
+    }
+
+    private static void initSsdNandFile() {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(SSD_NAND))) {
             StringBuilder sb = new StringBuilder();
             for (int i=0 ; i<100 ; i++)
                 sb.append(i + " " + "0x00000000" + '\n');
             bw.write(sb.toString());
         } catch (Exception e){
-
+            throw new RuntimeException();
         }
     }
 
-    public boolean write(int i, String s) {
-        if (i < 0 || i > 99) throw new RuntimeException();
-        if (!s.contains("0x")) throw new RuntimeException();
-        if (s.length() != 10) throw new RuntimeException();
-        char[] strArr = s.toCharArray();
-        for (int j = 2 ; j<strArr.length ; j++){
-            if (!((strArr[j] >= '0' && strArr[j] <= '9') || (strArr[j] >= 'A' && strArr[j] <= 'F'))) throw new RuntimeException();
-        }
-        String filePath = "ssd_nand.txt";
+    public boolean write(int lba, String newData) {
+        checkArgument(lba, newData);
         List<String> lines;
         try {
-            lines = Files.readAllLines(Paths.get(filePath));
+            lines = Files.readAllLines(Paths.get(SSD_NAND));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
-        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(SSD_NAND))) {
             StringBuilder sb = new StringBuilder();
             for (int j=0 ; j<100 ; j++){
-                if (j == i) sb.append(j + " " + s + '\n');
+                if (j == lba) sb.append(j + " " + newData + '\n');
                 else sb.append(lines.get(j) + '\n');
             }
             bw.write(sb.toString());
@@ -47,6 +48,32 @@ public class SSD {
         }
 
         return true;
+    }
+
+    private static void checkArgument(int lba, String newData) {
+        checkValidLBA(lba);
+        checkDataStartWith0x(newData);
+        checkDataLength(newData);
+        checkValidData(newData);
+    }
+
+    private static void checkValidData(String newData) {
+        char[] strArr = newData.toCharArray();
+        for (int j = 2 ; j<strArr.length ; j++){
+            if (!((strArr[j] >= '0' && strArr[j] <= '9') || (strArr[j] >= 'A' && strArr[j] <= 'F'))) throw new RuntimeException();
+        }
+    }
+
+    private static void checkDataLength(String newData) {
+        if (newData.length() != 10) throw new RuntimeException();
+    }
+
+    private static void checkValidLBA(int lba) {
+        if (lba < 0 || lba > 99) throw new RuntimeException();
+    }
+
+    private static void checkDataStartWith0x(String newData) {
+        if (!newData.startsWith("0x")) throw new RuntimeException();
     }
 
     public String read(int i) {
