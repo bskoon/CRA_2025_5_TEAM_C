69e3868a07fa3ac21c7d6bfe8dfb1ef062ef05c4
taeyeob-kim-09 <clockim@naver.com>
2025-06-11 15:04:01 +0900

    Update TestShellTest.java
    불필요한 부분 삭제

--- src/test/java/shell/TestShellTest.java
+++ src/test/java/shell/TestShellTest.java
@@ -1,3 +1,5 @@
+package shell;
+
 import org.junit.jupiter.api.*;
 import org.mockito.MockedStatic;
 import shell.TestShell;
@@ -18,30 +20,6 @@ public class TestShellTest {
         shell = spy(new TestShell());
     }
 
-    @Test
-    void testWriteAndReadSameLBA_withMock() {
-        // given
-        doReturn(true).when(shell).isValidLBA("5");
-        doReturn(true).when(shell).isValidValue("0xA1B2C3D4");
-        doNothing().when(shell).writeLBA(5, "0xA1B2C3D4");
-        doReturn("A1B2C3D4").when(shell).readLBA(5);
-
-        String input = String.join("\n",
-                "write 5 0xA1B2C3D4",
-                "read 5",
-                "exit"
-        );
-        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
-        //shell.scanner = new Scanner(inputStream);
-        shell.setScanner(new Scanner(inputStream));
-
-        // when
-        shell.launchShell();
-
-        // then
-        verify(shell).writeLBA(5, "0xA1B2C3D4");
-        verify(shell).readLBA(5);
-    }
 
     @Test
     void testInvalidCommandHandling() {
@@ -53,26 +31,6 @@ public class TestShellTest {
         shell.scanner = new Scanner(inputStream);
 
         shell.launchShell();
-    }
-
-    @Test
-    void testFullWriteAndFullRead_withMock() {
-        doReturn(true).when(shell).isValidValue("0xFFFFFFFF");
-        doReturn("FFFFFFFF").when(shell).readLBA(anyInt());
-
-        doNothing().when(shell).writeLBA(anyInt(), eq("0xFFFFFFFF"));
-
-        String input = String.join("\n",
-                "fullwrite 0xFFFFFFFF",
-                "fullread",
-                "exit"
-        );
-        shell.scanner = new Scanner(new ByteArrayInputStream(input.getBytes()));
-
-        shell.launchShell();
-
-        verify(shell, times(100)).writeLBA(anyInt(), eq("0xFFFFFFFF"));
-        verify(shell, times(100)).readLBA(anyInt());
     }
 
     @AfterEach
