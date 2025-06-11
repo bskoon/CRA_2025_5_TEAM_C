0660e367379c130816f7f120ebac833bb863938a
CJH <jae637@naver.com>
2025-06-11 13:30:59 +0900

    [test] thenReturn 이슈로 시나리오 변경 후 테스트 수정

--- src/main/java/shell/TestScenario.java
+++ src/main/java/shell/TestScenario.java
@@ -5,20 +5,21 @@ import java.util.Random;
 
 public class TestScenario {
     TestShell testShell;
+    Random rand;
 
-    public TestScenario(TestShell testShell) {
+    public TestScenario(TestShell testShell, Random rand) {
         this.testShell = testShell;
+        this.rand = rand;
     }
 
 
     private String readCompare(int i, String s) throws IOException {
         String result = testShell.readLBA(i);
-        if(!result.equals(s)) return "FAIL";
+            if(!result.equals(s)) return "FAIL";
         return "PASS";
     }
 
-    public String fullWriteAndReadCompare(long seed) throws IOException {
-        Random rand = new Random(seed);
+    public String fullWriteAndReadCompare() throws IOException {
         for(int i=0;i<20;i++){
             for(int j=0;j<5;j++){
                 String hexString = getRandomHexString(rand);
@@ -45,26 +46,23 @@ public class TestScenario {
         return "PASS";
     }
 
-    public String writeReadAging(long seed) throws IOException {
-        Random rand = new Random(seed);
-
-        for(int i=0;i<200;i++){
-            String hexString = getRandomHexString(rand);
-            testShell.writeLBA(0,hexString);
-            String result = readCompare(0,hexString);
-            if(result.equals("FAIL")) return result;
-            hexString = getRandomHexString(rand);
-            testShell.writeLBA(99,hexString);
-            result = readCompare(0,hexString);
-            if(result.equals("FAIL")) return result;
-        }
-        return "PASS";
-    }
-
     private String getRandomHexString(Random rand) {
         // 32비트 난수 생성 (0 ~ 0xFFFFFFFF)
         int randomValue = rand.nextInt(); // 기본적으로 -2^31 ~ 2^31-1 범위의 값
         // 16진수로 출력
         return "0x"+String.format("%08X", randomValue);
+    }
+
+    public boolean writeReadAgingOnce() throws IOException {
+        String hexString = getRandomHexString(rand);
+        testShell.writeLBA(0,hexString);
+        String result = readCompare(0,hexString);
+        if(result.equals("FAIL")) return false;
+        hexString = getRandomHexString(rand);
+        testShell.writeLBA(99,hexString);
+        result = readCompare(99,hexString);
+        if(result.equals("FAIL")) return false;
+
+        return true;
     }
 }
