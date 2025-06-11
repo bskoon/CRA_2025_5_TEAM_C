237b4f973b7bac58cddb2503c6d2005d75320725
CJH <jae637@naver.com>
2025-06-11 14:15:33 +0900

    [test] 신뢰성 확보를 위한 테스트 분리

--- src/test/java/shell/TestShellScenarioTest.java
+++ src/test/java/shell/TestShellScenarioTest.java
@@ -38,9 +38,9 @@ public class TestShellScenarioTest {
     }
 
     @Test
-    void launchShell_1_과1_풀네임_테스트(){
+    void launchShell_1_테스트(){
         // 1. 원하는 입력값을 문자열로 준비
-        String simulatedInput = "1_\n1_FullWriteAndReadCompare\nexit\n";
+        String simulatedInput = "1_\nexit\n";
         // 2. ByteArrayInputStream을 이용해 System.in을 시뮬레이션
         System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));  // System.in을 가짜 입력으로 변경
         testShell.launchShell();
@@ -48,9 +48,19 @@ public class TestShellScenarioTest {
         assertTrue(outputStream.toString().contains("FAIL")||outputStream.toString().contains("PASS"));
     }
     @Test
-    void launchShell_2_과2_풀네임_테스트(){
+    void launchShell_1_풀네임_테스트(){
         // 1. 원하는 입력값을 문자열로 준비
-        String simulatedInput = "2_\n2_PartialLBAWrite\nexit\n";
+        String simulatedInput = "1_FullWriteAndReadCompare\nexit\n";
+        // 2. ByteArrayInputStream을 이용해 System.in을 시뮬레이션
+        System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));  // System.in을 가짜 입력으로 변경
+        testShell.launchShell();
+
+        assertTrue(outputStream.toString().contains("FAIL")||outputStream.toString().contains("PASS"));
+    }
+    @Test
+    void launchShell_2_테스트(){
+        // 1. 원하는 입력값을 문자열로 준비
+        String simulatedInput = "2_\nexit\n";
         // 2. ByteArrayInputStream을 이용해 System.in을 시뮬레이션
         System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));  // System.in을 가짜 입력으로 변경
         testShell.launchShell();
@@ -59,9 +69,31 @@ public class TestShellScenarioTest {
 
     }
     @Test
-    void launchShell_3_과3_풀네임_테스트(){
+    void launchShell_2_풀네임_테스트(){
         // 1. 원하는 입력값을 문자열로 준비
-        String simulatedInput = "3_\n3_WriteReadAging\nexit\n";
+        String simulatedInput = "2_PartialLBAWrite\nexit\n";
+        // 2. ByteArrayInputStream을 이용해 System.in을 시뮬레이션
+        System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));  // System.in을 가짜 입력으로 변경
+        testShell.launchShell();
+
+        assertTrue(outputStream.toString().contains("FAIL")||outputStream.toString().contains("PASS"));
+
+    }
+    @Test
+    void launchShell_3_테스트(){
+        // 1. 원하는 입력값을 문자열로 준비
+        String simulatedInput = "3_\nexit\n";
+        // 2. ByteArrayInputStream을 이용해 System.in을 시뮬레이션
+        System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));  // System.in을 가짜 입력으로 변경
+        testShell.launchShell();
+
+        assertTrue(outputStream.toString().contains("FAIL")||outputStream.toString().contains("PASS"));
+
+    }
+    @Test
+    void launchShell_3_풀네임_테스트(){
+        // 1. 원하는 입력값을 문자열로 준비
+        String simulatedInput = "3_WriteReadAging\nexit\n";
         // 2. ByteArrayInputStream을 이용해 System.in을 시뮬레이션
         System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));  // System.in을 가짜 입력으로 변경
         testShell.launchShell();
