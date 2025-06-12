9bb74b2bd6535188168c3251ff0b91af444f01c6
bskoon <acedevil728@gmail.com>
2025-06-12 16:18:42 +0900

    [bugfix] erase test document import path fix

--- src/test/java/shell/TestShell_erase.java
+++ src/test/java/shell/TestShell_erase.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InOrder;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import shell.command.Document;
 import shell.command.EraseCommand;
 
 import java.io.ByteArrayOutputStream;
