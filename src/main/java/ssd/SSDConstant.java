0120219ddb4d71697d8b18262f8e78e6905616ef
CJH <jae637@naver.com>
2025-06-12 16:25:38 +0900

    [feature] buffer 폴더상수 추가

--- src/main/java/ssd/SSDConstant.java
+++ src/main/java/ssd/SSDConstant.java
@@ -3,6 +3,7 @@ package ssd;
 public class SSDConstant {
     public static String SSD_FILE_PATH = "ssd_nand.txt";
     public static String OUTPUT_FILE_PATH = "ssd_output.txt";
+    public static String BUFFER_FOLDER_PATH = "buffer";
     public static final char READ = 'R';
     public static final char WRITE = 'W';
     public static final char ERASE = 'E';
