34ee37fc01836c7730ea7ee29e0d1763bad49cbc
Jinchan <ghkdwlscks@gmail.com>
2025-06-13 14:54:56 +0900

    [refactoring]AddLog2

--- src/main/java/shell/TestShell.java
+++ src/main/java/shell/TestShell.java
@@ -140,7 +140,7 @@ public class TestShell {
     }
 
     public static void main(String[] args) {
-        if (args != null)
+        if (args.length != 0)
             new Runner(args, initCommandExecutor()).run();
         else {
             TestShell shell = new TestShell();
